c3a5dac6d4c2fd9a8b69f5fbdeff7126ceec29e7
unknown <felipe.monobe@NBQ44-051.fcamara.com.br>
2015-06-17 20:18:12 -0300

    implementing productService

--- public/scripts/controllers/productController.js
+++ public/scripts/controllers/productController.js
@@ -2,26 +2,37 @@
 	angular
 	.module('app')
 	.controller('ProductController', productController);
+	
+	productController.$inject = ['productService'];
 		
-	function productController() {
+	function productController(productService) {
 		var vm = this;
 		vm.products = [];
+		vm.fetchProduct = fetchProduct;
 		
-		vm.products = [
-			{
-				name: 'Product #1',
-				stock: 23,
-				description: 'Blabla',
-				brand: 'Tora',
-				price: 53.22
-			},
-			{
-				name: 'Product #2',
-				stock: 5,
-				description: 'Blabla',
-				brand: 'Xong',
-				price: 33.28
-			}
-		];
+		function fetchProduct() {
+			return productService.getProduct()
+			.then(function(data) {
+				vm.products = data;
+				return vm.products;
+			});
+		}
+		
+		// vm.products = [
+		// 	{
+		// 		name: 'Product #1',
+		// 		stock: 23,
+		// 		description: 'Blabla',
+		// 		brand: 'Tora',
+		// 		price: 53.22
+		// 	},
+		// 	{
+		// 		name: 'Product #2',
+		// 		stock: 5,
+		// 		description: 'Blabla',
+		// 		brand: 'Xong',
+		// 		price: 33.28
+		// 	}
+		// ];
 	}	
 })();
--- public/scripts/services/productService.js
+++ public/scripts/services/productService.js
@@ -3,7 +3,25 @@
 	.module('app')
 	.factory('productService', productService);
 	
-	function productService() {
+	productService.$inject = ['$http'];
+	
+	function productService($http) {
+		return {
+			getProduct: getProduct
+		};
 		
+		function getProduct() {
+			return $http.get('/api/getProduct')
+			.then(getProductSuccess)
+			.catch(getProductError);
+			
+			function getProductSuccess(response) {
+				return response.data.results;
+			}
+			
+			function getProductError(error) {
+				console.log('XHR failed for getProduct. ' + error.data);
+			}
+		}
 	}
 })();
