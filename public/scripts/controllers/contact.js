73caa95dfe399ef6ae388c3d6f2a89dc10d52f6c
FelipeMonobe <xinube@gmail.com>
2015-07-24 17:08:41 -0300

    pause

--- public/scripts/controllers/contact.js
+++ public/scripts/controllers/contact.js
@@ -10,5 +10,25 @@
       shell = $rootScope;
 
     shell.title = 'Contact';
+    vm.googleMaps_Load = googleMaps_Load;
+
+    (function init() {
+      vm.googleMaps_Load();
+    })();
+
+    function googleMaps_Load() {
+      var map = new google.maps.Map(document.getElementById('map-canvas'), {
+        zoom: 15,
+        center: {
+          lat: -23.596924,
+          lng: -46.728920
+        }
+      });
+
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(-23.596924,-46.728920),
+        map: map
+      });
+    }
   }
 })();
