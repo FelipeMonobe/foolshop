a801a036cbd2db25801453f165d38e82cde107d2
FelipeMonobe <xinube@gmail.com>
2015-07-27 13:02:39 -0300

    env port fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('./config/database')('mongodb://localhost/foolshop');
 
 var express = require('express'),
   app = require('./config/express')(app),
-  port = process.env.ENV_PORT || 1337;
+  port = process.env.PORT || 5000;
 
 app.listen(port);
 
